0a8e9504ac6ab706c3d4788b836fba0e523343f7
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-06-17 22:55:02 -0400

    Server code pushed.

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,3 +1,17 @@
 import jwt from "jsonwebtoken";
 
 // Middleware that verifies a user's token.
+const verifyToken = (req, res, next) => {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, "mytokensecret", (err, decoded) => {
+        if (err) {
+            console.log(err);
+            res.json({ message: "Authentication error." });
+        } else {
+            req.user = decoded; // decoded is the payload object, could be also named 'user'.
+            next();
+        }
+    });
+}
+
+export default verifyToken;
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,28 +2,51 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import Post from "../models/Post.js";
+import verifyToken from "../middlewares/authMiddleware.js";
 import mongoose from "mongoose";
 
 const router = express.Router();
 
-// GET Route for loading the login page.
+// GET Route that provides user's data.
+router.get("/user-data/:id", verifyToken, async (req, res) => {
+    const userId = req.params.id;
+    const user = await User.find({ _id: userId });
+    return res.json({ user: user[0] });
+});
 
 // POST Route when the user hits the login button.
+router.post("/authenticating", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const loggedUser = await User.find({ username, password });
+    console.log(loggedUser[0]);
+    if (loggedUser.length === 0) {
+        return res.json({ message: "Invalid credentials." })
+    } else {
+        const payload = { username: loggedUser[0].username, password: loggedUser[0].password, id: loggedUser[0]._id }
+        const token = jwt.sign(payload, "mytokensecret");
+        res.json({ token, userData: { id: loggedUser[0]._id } });
+    }
+});
+
 
 // GET Route for loading the logged user's posts.
-router.get("/posts", async (req, res) => {
-    const user = await User.find({});
-    return res.json(user);
+router.get("/posts", verifyToken, async (req, res) => {
+    const posts = await Post.find({});
+    return res.json(posts);
 });
 
 // POST Route for when the user uploads a post.
-router.post("/send-post", async (req, res) => {
-    const postContent = "hy";
-    const user = await User.find({ username: "ibrah" });
-    const newPost = new Post({ content: postContent, postByUser: user[0]._id });
+router.post("/send-post", verifyToken, async (req, res) => {
+    const postContent = req.body.content;
+    const userID = req.user.id;
+
+    const newPost = new Post({ content: postContent, postByUser: userID });
     await newPost.save();
-    await User.updateOne({ _id: user[0]._id }, { $push: { userPost: newPost._id } });
-    return res.json({ post: newPost, user });
+
+    await User.updateOne({ _id: userID }, { $push: { userPost: newPost._id } });
+    return res.json({ post: newPost, userID });
 })
 
 export default router;
